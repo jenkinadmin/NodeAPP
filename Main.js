cb5e7e0fb64d7d76bf1b302085b4217da4ae71a7
Ninad.Tamore <ninad_tamore@rediffmail.com>
2021-06-25 19:53:37 +0530

    Adding main.js

--- Main.js
+++ Main.js
@@ -5,5 +5,6 @@ const db="abc";
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.end('Hello World!' + db);
+  
 }).listen(8088);
