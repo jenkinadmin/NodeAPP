56d95ab9778f747ea32afa1369cb151da24d1794
Ninad.Tamore <ninad_tamore@rediffmail.com>
2021-06-25 19:10:24 +0530

    changed in main.js

--- Main.js
+++ Main.js
@@ -1,5 +1,8 @@
 var http = require('http');
 var express = require('express');
+
+const db="abc";
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
