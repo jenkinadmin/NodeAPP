9216b6f8afd1ac4acfdc51fea866e994907d7898
Ninad.Tamore <ninad_tamore@rediffmail.com>
2021-06-29 15:51:41 +0530

    changes in Main.js

--- Main.js
+++ Main.js
@@ -2,10 +2,12 @@ var http = require('http');
 var express = require('express');
 
 const db="abc";
+const port=3000;
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!' + db);
-  
-}).listen(3000);
+var app =express();
+
+
+app.listen(port,function(){
+	console.log('MAP API Server started at port:' + port);
+});
 
